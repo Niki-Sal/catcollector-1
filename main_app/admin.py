2a8be801c055dd7f52e30be6eb8a393721dc1b5a
niki-sal <ne.salehi@gmail.com>
2021-03-25 10:40:29 -0700

    basic Django authentication setup

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+# import your models here
+from .models import Cat, Feeding
+
+# Register your models here
+admin.site.register(Cat)
+admin.site.register(Feeding)
